f3a511fac5b3d6e3796f753b91ad39e0628ede2e
apolloyan <574566334@qq.com>
2016-02-19 16:24:32 +0800

    test branch on ff

--- TestInjector/TestInjector/Program.cs
+++ TestInjector/TestInjector/Program.cs
@@ -112,7 +112,7 @@ namespace TestInjector
             stopwatch.Stop();
             Console.WriteLine("simpleWithInterceptor:" + stopwatch.ElapsedMilliseconds);
 
-            Console.WriteLine("end_branch");
+            Console.WriteLine("end_branch_fenzhi");
 
             Console.Write("end_test");
 
